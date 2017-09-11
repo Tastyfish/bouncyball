21bc973e4a919fe00921c9a14ba66f09cdea374e
Tastyfish <crazychris32@gmail.com>
2017-09-10 20:04:52 -0400

    background plane, also more readable UpdateBall

--- entities/ent_ball.c
+++ entities/ent_ball.c
@@ -5,6 +5,10 @@
 #include "ent_ball.h"
 #include "gent.h"
 
+#define PF_MOVE_LEFT (1)
+#define PF_MOVE_UP (2)
+#define PF_MOVE_MASK (PF_MOVE_LEFT|PF_MOVE_UP)
+
 void UpdateBall(Entity* entity);
 
 void ent_Ball(Entity* entity) {
@@ -30,33 +34,33 @@ void UpdateBall(Entity* entity) {
 	Sprite* s = SpriteIDToAddress(entity->a);
 
 	// movement
-	if(entity->b & 1) {
+	if(entity->b & PF_MOVE_LEFT) {
 		s->x--;
 
 		if(s->x <= 1)
-			entity->b &= ~1;
+			entity->b &= ~PF_MOVE_LEFT;
 	} else {
 		s->x++;
 
 		if(s->x >= 248)
-			entity->b |= 1;
+			entity->b |= PF_MOVE_LEFT;
 	}
 
-	if(entity->b & 2) {
+	if(entity->b & PF_MOVE_UP) {
 		s->y--;
 
 		if(s->y <= 8)
-			entity->b &= ~2;
+			entity->b &= ~PF_MOVE_UP;
 	} else {
-		// acceleration
+		// acceleration in the upper nibble of B
 		s->y += (entity->b >> 2);
 
 		if(entity->b < 0xFC)
 			entity->b += 4;
 
 		if(s->y >= 224) {
-			entity->b |= 2;
-			entity->b &= 3;
+			entity->b |= PF_MOVE_UP;
+			entity->b &= PF_MOVE_MASK;
 		}
 	}
 }
--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include "ent_ball.h"
 
 extern void g_Init();
+extern void __fastcall__ v_DecompressNT(int ppuAddr, void* ptr);
+extern char NAM_BG;
 
 void main() {
 	int i;
@@ -16,13 +18,21 @@ void main() {
 	v_SetPalette(PAL_SPR0, 0x16, 0x06, 0x36);
 	v_SetPalette(PAL_SPR1, 0x12, 0x02, 0x32);
 	v_SetPalette(PAL_SPR2, 0x19, 0x09, 0x39);
-	v_SetPalette(PAL_SPR3, 0x17, 0x07, 0x37);
+	v_SetPalette(PAL_SPR3, 0x14, 0x04, 0x34);
+
+	v_SetPalette(PAL_BG0, 0x01, 0x10, 0x20);
+	v_SetPalette(PAL_BG1, 0x01, 0x21, 0x31);
+	v_SetPalette(PAL_BG2, 0x17, 0x27, 0x37);
+	v_SetPalette(PAL_BG3, 0x01, 0x27, 0x37);
+
+	v_DecompressNT(0x2000, &NAM_BG);
 
 	for(i = 0; i < 64; i++) {
 		e_Create(&ent_Ball);
 	}
 
 	v_EnableSprites(true);
+	v_EnableBackgrounds(true);
 
 	while(true) {
 		v_FullCopyOAM();
