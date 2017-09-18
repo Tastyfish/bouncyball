29ea451c4efd70db1ffb70d47f0289e10a0fbd44
Tastyfish <crazychris32@gmail.com>
2017-09-18 00:21:07 -0400

    Header change that should have been added with the MMC5 updates

--- include/mmc5.h
+++ include/mmc5.h
@@ -4,3 +4,6 @@ typedef unsigned char line_t;
 
 HScanlineCB vm_AddScanlineCallback(line_t line, ScanlineCallback callback);
 void vm_RemoveScanlineCallback(HScanlineCB cb);
+
+void __fastcall__ vm_SetNametableMirroring(char code);
+void __fastcall__ vbm_SetNametableMirroring(char code);
