0f62e0f763fa7f5fc8f8e7fca5642b9dc804e21f
Tastyfish <crazychris32@gmail.com>
2017-10-18 00:50:01 -0400

    XData tokens and insertion into pinball map

--- res/res.s
+++ res/res.s
@@ -7,12 +7,12 @@ symbol:
 	.addr data
 .endmacro
 
-.macro incmap symbol, filename, sx, sy
+.macro incmap symbol, filename, sx, sy, xdata
 	.local data, qrv, qrc, qre
 	.export symbol
 data:
 	.word sx, sy
-	.addr qrv, qrc, qre
+	.addr qrv, qrc, qre, xdata
 qrv:
 	.incbin .sprintf("%s.qrv", filename)
 qrc:
@@ -41,8 +41,22 @@ incres _NAM_BG, "bg.rle"
 incres _NAM_JOKE, "joke.rle"
 incres _NAM_DIRECTIONS, "directions.rle"
 
-incmap _MAP_TITLE, "title", 128, 0
-incmap _MAP_PINBALL1, "pinball1", 27*8, 29*8
+incmap _MAP_TITLE, "title", 128, 0, 0
+
+; Mostly for infos
+.scope mapx_pinball
+start:
+	.addr str0, str1, str2
+str0:
+	.byte "Use the D-Pad to move the ball around.",0
+str1:
+	.byte "Coins give you points.",0
+str2:
+	.byte "Sometimes there are multiple paths, but it may be harder to use one than the other.", 0
+.endscope
+
+incmap _MAP_PINBALL1, "pinball1", 27*8, 29*8, mapx_pinball::start
+
 
 .export _S_MUSIC
 _S_MUSIC:
